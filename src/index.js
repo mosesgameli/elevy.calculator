4a69da7f922b7a1c76cf4da92e0390c36f3e371d
moses-20 <mosesgameli20@gmail.com>
2022-05-26 16:08:47 +0000

    temp remove perf from reportwebvitals

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { perf } from "./firebase.config";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -21,4 +20,4 @@ serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(perf);
+reportWebVitals();
