4b4970fcef7c6e4aeacf1be06602b0b162ba27d4
moses-20 <mosesgameli20@gmail.com>
2022-05-26 16:12:20 +0000

    pass perf to reportwebvitals

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { perf } from "./firebase.config";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -20,4 +21,4 @@ serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(perf);
